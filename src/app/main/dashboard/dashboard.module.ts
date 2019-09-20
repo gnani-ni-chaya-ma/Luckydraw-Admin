b733e1e78c2544f89920f5f9772e439a477253e7
Milan Vadher <milanvadher1996@gmail.com>
2019-09-20 18:44:35 +0530

    add charts on dashboard

--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -3,12 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseWidgetModule } from '@fuse/components';
+
+import { ChartsModule } from 'ng2-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSelectModule,
+
+    ChartsModule,
+    NgxChartsModule,
+
+    FuseSharedModule,
+    FuseWidgetModule
   ]
 })
 export class DashboardModule { }
--- src/app/main/dashboard/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations,
 })
 export class DashboardComponent implements OnInit {
+
+  datasets = [
+    {
+      label: 'Visitors',
+      data: [190, 300, 340, 220, 290, 390, 250, 380, 410, 380, 320, 290],
+      fill: 'start'
+
+    },
+    {
+      label: 'Page views',
+      data: [2200, 2900, 3900, 2500, 3800, 3200, 2900, 1900, 3000, 3400, 4100, 3800],
+      fill: 'start'
+    }
+  ];
+
+  labels = ['12am', '2am', '4am', '6am', '8am', '10am', '12pm', '2pm', '4pm', '6pm', '8pm', '10pm'];
+
+  options = {
+    spanGaps: false,
+    legend: {
+      display: false
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+      position: 'nearest',
+      mode: 'index',
+      intersect: false
+    },
+    layout: {
+      padding: {
+        left: 24,
+        right: 32
+      }
+    },
+    elements: {
+      point: {
+        radius: 4,
+        borderWidth: 2,
+        hoverRadius: 4,
+        hoverBorderWidth: 2
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            fontColor: 'rgba(0,0,0,0.54)'
+          }
+        }
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            tickMarkLength: 16
+          },
+          ticks: {
+            stepSize: 1000
+          }
+        }
+      ]
+    },
+    plugins: {
+      filler: {
+        propagate: false
+      }
+    }
+  };
+
+  chartType = 'line';
+
+  widget = {
+    'currentRange': 'TW',
+    'title': 'Task Distribution',
+    'colorScheme': 'picnic',
+    'ranges': {
+      'TW': 'This Week',
+      'LW': 'Last Week',
+      '2W': '2 Weeks Ago'
+    },
+    'mainChart': {
+      'TW': [
+        {
+          'name': 'Frontend',
+          'value': 15
+        },
+        {
+          'name': 'Backend',
+          'value': 20
+        },
+        {
+          'name': 'API',
+          'value': 38
+        },
+        {
+          'name': 'Issues',
+          'value': 27
+        }
+      ],
+    },
+  };
 
   constructor() { }
 
